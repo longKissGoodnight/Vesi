3b5d0a57e97d498e8434e18244f7cc9c39853266
Dima <covid.20v2.0.2.0@gmail.com>
2024-10-24 17:20:38 +0300

    кнопки

---
+++ FoodFlow/Repos/OrdersRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using FoodFlow.Models;
+
+namespace FoodFlow.Repos
+{
+    internal class OrderRepository
+    {
+        public IEnumerable<Order> GetAll()
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                WriteIndented = true
+            };
+
+            //var json = File.ReadAllText("Menu\\Order.json");
+            //return JsonSerializer.Deserialize<List<Order>>(json, options);
+            return JsonSerializer.Deserialize<List<Order>>(System.IO.File.ReadAllText("Menu\\Order.json"), options);
+        }
+    }
+}
---
+++ FoodFlow/ViewModels/HistoreOrderViewModel.cs
@@ -0,0 +1,47 @@
+﻿using FoodFlow.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FoodFlow.ViewModels.Layout
+{
+    public class HistoryOrderViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Order> _orders;
+
+        public ObservableCollection<Order> Orders
+        {
+            get => _orders;
+            set
+            {
+                if (_orders != value)
+                {
+                    _orders = value;
+                    OnPropertyChanged(nameof(Orders));
+                }
+            }
+        }
+
+        public HistoryOrderViewModel()
+        {
+            LoadOrders();
+        }
+
+        private void LoadOrders()
+        {
+            // Пример загрузки данных из JSON-файла
+            string json = File.ReadAllText("Menu\\Order.json");
+            var orders = JsonSerializer.Deserialize<ObservableCollection<Order>>(json);
+            Orders = orders ?? new ObservableCollection<Order>();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
